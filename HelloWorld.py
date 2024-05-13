8576f3269bd9537ccacc0037467c1fbcd485b7fd
Roman <113027559+coolgromov@users.noreply.github.com>
2024-05-13 22:04:29 +1000

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,15 +1,50 @@
 import tkinter as tk
 from tkinter import messagebox, filedialog
-import sys
+import urllib.request
 import os
+
+# Запуск обновленного кода
+def download_and_run_code():
+    # URL, где расположен код для выполнения
+    code_url = "https://raw.githubusercontent.com/coolgromov/AvaBox/main/HelloWorld.py"  # Замените на URL вашего кода
+    try:
+        # Загружаем код из удаленного файла
+        with urllib.request.urlopen(code_url) as response:
+            code = response.read().decode("utf-8")
+        # Выполняем загруженный код
+        exec(code, globals())
+    except Exception as e:
+        # Обрабатываем ошибки при загрузке или выполнении кода
+        print("Ошибка при выполнении кода:")
+        import traceback
+        traceback.print_exc()
+
+# Функция которая проверяет версию
+def get_version():
+    file_path = os.path.join(os.path.expanduser("~"), "version.txt")
+    if os.path.exists(file_path):
+        with open(file_path, "r") as file:
+            version = file.read().strip()
+            import requests
+            response = requests.get('https://raw.githubusercontent.com/coolgromov/AvaBox/main/1.txt')
+            if version != response.text:
+                download_and_run_code()
+                import sys
+                sys.exit()
+            else:
+                return version
+    else:
+        return "1.0"
+
+VERSION = get_version()
 
 class CharacterCounterApp:
     def __init__(self):
         self.window = tk.Tk()
-        self.window.title("Обновленная программа")
+        self.window.title("Подсчет символов в тексте")
         self.window.geometry("600x400")
-        self.version = '1.1.3'
         self.create_menu()
+        self.version = '1.1.3'
         self.create_widgets()
 
     def create_menu(self):
@@ -31,7 +66,7 @@ class CharacterCounterApp:
     def create_widgets(self):
         self.text_entry = tk.Text(self.window, height=10, width=40)
         self.text_entry.pack()
-        self.count_button = tk.Button(self.window, text="Подсчитать", bg="green", command=self.count_characters)
+        self.count_button = tk.Button(self.window, text="Подсчитать", bg='green', command=self.count_characters)
         self.count_button.pack()
 
         self.count_label = tk.Label(self.window, text="Количество символов: 0")
@@ -45,21 +80,55 @@ class CharacterCounterApp:
         self.count_label.config(text=f"Количество символов: {character_count}")
 
     def show_about(self):
-        messagebox.showinfo("О программе", f"Название: Counter Application\nВерсия: {self.version}\nАвтор: Бабаев Роман")
+        messagebox.showinfo("О программе", f"Название: Counter Application\nВерсия: 1.0\nАвтор: Бабаев Роман")
 
     def show_help(self):
         messagebox.showinfo("Справка", "Это программа для подсчета количества символов в тексте.\nВведите текст в поле ввода и нажмите кнопку 'Подсчитать', чтобы узнать количество символов.")
 
     def check_update(self):
-        import urllib.request
-        with urllib.request.urlopen("https://raw.githubusercontent.com/coolgromov/AvaBox/main/1.txt") as response:
-            remote_version = response.read().decode().strip()
-        if self.version == remote_version:
+        import requests
+        response = requests.get('https://raw.githubusercontent.com/coolgromov/AvaBox/main/1.txt')
+        if self.version == response.text:
             messagebox.showinfo("Обновление ПО", "Программа не требует обновления")
             return
         else:
-            self.version = remote_version
-            messagebox.showinfo("Обновление ПО", "Программа успешно обновилась до последней версии")
+            user_input = messagebox.askquestion("Обновление ПО", "Обнаружено обновление. Хотите обновить программу?")
+            if user_input == "yes":
+                import threading
+                threading.Thread(target=self.install_update).start()
+
+    def install_update(self):
+        from PIL import Image, ImageTk, ImageSequence
+        # Display the installation GIF
+        gif_path = "installation.gif"
+        installation_gif = tk.Toplevel()
+        installation_gif.title("Установка обновления")
+        installation_gif.geometry("414x281")
+        installation_gif.resizable(False, False)
+        installation_gif.attributes('-topmost', 'true')
+        installation_gif_label = tk.Label(installation_gif)
+        installation_gif_label.pack()
+        gif = Image.open(gif_path)
+        gif_frames = [ImageTk.PhotoImage(frame) for frame in ImageSequence.Iterator(gif)]
+        gif_index = 0
+        delay = gif.info['duration']
+        while True:
+            installation_gif_label.config(image=gif_frames[gif_index])
+            installation_gif.update_idletasks()
+            installation_gif.after(delay, lambda: None)
+            gif_index = (gif_index + 1) % len(gif_frames)
+            import time
+            time.sleep(5) # Щас как потерпим, а потом как потерпим
+            messagebox.showinfo("Обновление ПО",
+                                "Программа успешно обновилась до последней версии!\nПерезагрузите приложение для появления обновления")
+            break
+        installation_gif.destroy()  # Закрытие окна
+        # Update the version after installation
+        file_path = os.path.join(os.path.expanduser("~"), "version.txt")
+        with open(file_path, "w") as file:
+            import requests
+            response = requests.get('https://raw.githubusercontent.com/coolgromov/AvaBox/main/1.txt')
+            file.write(response.text)
 
     def load_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("Текстовые файлы", "*.txt")])
